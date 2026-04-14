9b0ad887cbc654a4d2f9be2b60c44f014d5454e2
shashvatyadav-042 <shashvat1012007@gmail.com>
2026-04-14 23:20:53 +0530

    Implement sum calculation based on user input

---
+++ question5.js
@@ -0,0 +1,16 @@
+let N = parseInt(prompt("Enter N:"));
+let seed = parseInt(prompt("Enter seed:"));
+
+let divisor = seed + 2;
+let sum = 0;
+let m = 0;
+
+while (sum < N) 
+{
+    m++;
+    if (m % divisor !== 0) 
+    {
+        sum += m;
+    }
+}
+alert(m + " " + sum);
